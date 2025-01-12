ed1ee119567a1aba2b66e53e6b9ced9d41f869a5
GaziTayeb75 <gazitayeb75@gmail.com>
2025-01-12 13:22:46 -0800

    signup success
    signup success

--- src/to_do_list/FXMLDocumentController.java
+++ src/to_do_list/FXMLDocumentController.java
@@ -5,7 +5,17 @@
 package to_do_list;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -82,14 +92,109 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private PasswordField changePass_password;
     
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+   private Connection connect;
+   private PreparedStatement prepare;
+   private ResultSet result;
+   private Statement statement;
+   
+   public Connection connectDB(){
+       
+       try{
+           Class.forName("com.mysql.jdbc.Driver");
+           Connection connect
+                   = DriverManager.getConnection("jdbc:mysql://localhost/useraccount","root","");
+           return connect;
+       }catch(Exception e){e.printStackTrace();}
+  return null;
+   }
+   
+   
+   
+   public void login(){
+       
+   }
+   
+   public void register(){
+       alertMessage alert = new alertMessage();
+       //check we have empty fields
+       
+       if(signup_email.getText().isEmpty() || signup_username.getText().isEmpty()
+               || signup_password.getText().isEmpty() || signup_cPassword.getText().isEmpty()
+               || signup_selectQuestion.getSelectionModel().getSelectedItem() == null
+               || signup_answer.getText().isEmpty()){
+           alert.errorMessage("All fields are necessary to be filled ");
+       } else if(signup_password.getText() == signup_cPassword.getText()){  
+           //check if the value of password fields is equal to confirm password
+           alert.errorMessage("Password does not match");
+           
+       }else if(signup_password.getText().length()<8){
+          //check tf the length of password value if less then to 8, check The characters of password 
+       
+      alert.errorMessage("invalid Password,at least 8 characters needed");
+       
+       }else{
+           //check if the username is already taken
+           String checkUsername = "SELECT * FROM users WHERE username ='"
+                   +signup_username.getText()+"'";
+            connect = connectDB();
+             try{
+                  statement = connect.createStatement();
+           result = statement.executeQuery(checkUsername);
+           
+           if(result.next()){
+               alert.errorMessage(signup_username.getText() + "is already taken");
+           }else{
+               
+               String insertData = "INSERT INTO USER "
+                    +"(email,username,password,question,answer,date)"
+                    +"VALUES(?,?,?,?,?,?)";//
+               
+               prepare = connect.prepareStatement(insertData);
+              prepare.setString(1, signup_email.getText());
+               prepare.setString(2, signup_username.getText());
+               prepare.setString(3, signup_password.getText());
+               prepare.setString(4,
+                       (String) signup_selectQuestion.getSelectionModel().getSelectedItem());
+               prepare.setString(5, signup_answer.getText());
+              
+               Date date = new Date();
+               java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+               
+               prepare.setString(6, String.valueOf(sqlDate));
+               
+               prepare.executeUpdate();
+               
+               alert.successMessage("Registered Successfully!!");
+               
+               
+           
+           }
+             }catch(Exception e){
+                 e.printStackTrace();
+             
+          }
+          
+       }
+         
+   }
+   
+   private String[] questionList = {"What is your favorite food?","What is your favorite color?",
+         "What is the name of your pet?", "What is your most favorite sport?"};
+   
+           public void questions(){
+              List<String> listQ = new ArrayList<>();
+              
+              for(String data: questionList){
+                  listQ.add(data);
+              }
+              ObservableList listData = FXCollections.observableArrayList(listQ);
+         signup_selectQuestion.setItems(listData);
+           }
+
+ 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+       questions();
     }    
     
 }
